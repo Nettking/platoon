c8f615233e9b0d1f754ff71b52c4dcfe67d0f677
Nettking <m@nettking.no>
2023-02-28 21:49:16 +0100

    add basic platoon stuff

---
+++ platooning/__init__.py
@@ -0,0 +1,2 @@
+from platooning.get_distance import *
+from platooning.initialize_distance_sensor import *
---
+++ platooning/get_distance.py
@@ -0,0 +1,29 @@
+# import the GoPiGo3 drivers
+import time
+from platooning import *
+
+
+
+
+def get_distance(my_distance_sensor):
+    try:
+        distance_in_mm = str(my_distance_sensor.read_mm())
+        return distance_in_mm
+    except:
+        print("Distance sensor reading error")
+        return None
+    
+
+
+if __name__ == '__main__':
+
+    #initialize distance sensor
+    my_distance_sensor = initialize_distance_sensor()
+    
+    # Continue reading the values from the sensor until the user presses CTRL+C
+    while True:
+        # Read the distance value from the sensor and convert it to a string.
+        distance_in_mm = get_distance(my_distance_sensor)
+
+        # Directly print the values of the sensor.
+        print("Distance Sensor Reading (mm): " + distance_in_mm)
