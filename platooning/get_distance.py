80c5d29c6b92b7d168c3d732d017ff505b8d4383
Nettking <m@nettking.no>
2023-03-01 23:31:05 +0100

    Added Comments

--- platooning/get_distance.py
+++ platooning/get_distance.py
@@ -2,9 +2,15 @@ import time
 from platooning import initialize_distance_sensor
 
 def get_distance(my_distance_sensor):
+    # Read the distance value from the sensor and return it to a string.
     try:
+        # Read the distance value from the sensor and convert it to a string.
         distance_in_mm = str(my_distance_sensor.read_mm())
+        
+        # Return the distance value
         return distance_in_mm
+    
+    # If there is an error reading the distance value, print an error message and return None
     except:
         print("Distance sensor reading error")
         return None
