d4b88c64f65d98a5b3f3e9503d4a603e110361b9
Nettking <m@nettking.no>
2023-03-02 11:26:44 +0100

    Added stop and adjust speed if too close distance

--- run.py
+++ run.py
@@ -112,12 +112,16 @@ while True:
         print('Wheel speeds: ' + str(leftSpeed) + str(rightSpeed))
         
         
-        # Get distance
+        # Get distance and adjust speed if too close
         distance = get_distance(myDistanceSensor)
         if distance is not None:
             print('Distance: ' + distance)
             if int(distance) < 100:
                 gpg.set_speed(0)
+            elif int(distance) < 200:
+                gpg.set_speed(50)
+            else:
+                gpg.set_speed(100)
 
 
         key = cv2.waitKey(1)
