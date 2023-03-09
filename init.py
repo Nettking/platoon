2951abedf38b19a27e028b3fcd86c33f200bfcd3
Nettking <m@nettking.no>
2023-03-09 12:59:24 +0100

    Update init.py

--- init.py
+++ init.py
@@ -17,6 +17,6 @@ def init():
 
     # Initialize video capture and set resolution
     video = cv2.VideoCapture(0)
-    video.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-    video.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+    video.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
     return video, gpg, myDistanceSensor
