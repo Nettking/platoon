d8bcc0c09e47512ba796608958305ba480d11acb
Nettking <m@nettking.no>
2023-03-09 02:55:26 +0100

    Cleaner version

--- platooning/calculate_steering_angle.py
+++ platooning/calculate_steering_angle.py
@@ -1,6 +1,6 @@
 from math import atan2
+
 wheelbase = 0.117  # distance between wheels in meters
-max_speed = 100  # maximum speed in meters per second
 
 def calculate_steering_angle(x_offset):
     # Calculate steering angle in radians
