5c9a025304180c66ef229a0ba02d089109aca2a6
Nettking <m@nettking.no>
2023-03-15 15:29:59 +0100

    Update PlatoonVehicle.py

--- PlatoonVehicle.py
+++ PlatoonVehicle.py
@@ -165,7 +165,7 @@ class PlatoonVehicle:
         print('entered avg')
         # Initialize the list of lane lines
         lane_lines = []
-        print('lane_lines init')
+        
         # If no line segments were detected, stop the robot and return an empty list of lane lines
         if line_segments is None:
             print("no line segments detected")
@@ -173,11 +173,11 @@ class PlatoonVehicle:
 
         # Get the height and width of the input image
         height, width, _ = frame.shape
-        print('frame shape')
+        
         # Initialize the lists of left and right line fits
         left_fit = []
         right_fit = []
-        print('fits lists')
+        
         # Define the region of interest for the left and right lane lines
         boundary = 1/2
         # The left region of interest is the left half of the image
@@ -187,10 +187,10 @@ class PlatoonVehicle:
 
         # Iterate over all line segments and fit a line to each segment
         for line_segment in line_segments:
-            print('line segments iteration')
+            
             # Iterate over the endpoints of the line segment
             for x1, y1, x2, y2 in line_segment:
-                print('Iterating...')
+                
                 # Skip vertical lines (slope = infinity)
                 if x1 == x2:
                     print("skipping vertical lines (slope = infinity")
@@ -200,10 +200,10 @@ class PlatoonVehicle:
                 #fit = np.polyfit((x1, x2), (y1, y2), 1)
                 # Calculate the slope and intercept of the line
                 slope = (y2 - y1) / (x2 - x1)
-                print('slope')
+                
                 # Calculate the intercept of the line
                 intercept = y1 - (slope * x1)
-                print('intercept')
+                
                 # Classify the line segment as left or right based on the slope and x-coordinate of endpoints
                 if slope < 0:
                     # If the slope is negative, the line segment is on the left side of the image
@@ -213,7 +213,7 @@ class PlatoonVehicle:
                     # If the slope is positive, the line segment is on the right side of the image
                     if x1 > right_region_boundary and x2 > right_region_boundary:
                         right_fit.append((slope, intercept))
-        print('Done iterating')
+        
         # Calculate the average slope and intercept for the left and right line fits
         left_fit_average = np.average(left_fit, axis=0)
         # If there are no left line fits, return an empty list of lane lines
@@ -230,7 +230,7 @@ class PlatoonVehicle:
 
 
     def display_lines(self, frame, lines, line_color=(0, 255, 0), line_width=6):
-
+        print('entered display lines')
 
         line_image = np.zeros_like(frame)
         
