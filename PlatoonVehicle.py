4d2f5004ec9cf75f46d2db3ddb76b331b6b636bf
Nettking <m@nettking.no>
2023-03-22 10:40:19 +0100

    Update PlatoonVehicle.py

--- PlatoonVehicle.py
+++ PlatoonVehicle.py
@@ -34,33 +34,28 @@ class PlatoonVehicle:
         self.MQTT_TOPIC_PUB = MQTT_TOPIC_PUB
     
     
-    def initialize_distance_sensor(gpg):
-        '''
-        Create an instance of the Dstance Sensor class.
-        I2C1 and I2C2 are just labels used for identifyng the port on the GoPiGo3 board.
-        But technically, I2C1 and I2C2 are the same thing, so we don't have to pass any port to the constructor.
-        '''
-        my_distance_sensor = gpg.init_distance_sensor()
-        return my_distance_sensor
-
-
     def init(self):
         # Initialize GoPiGo3 robot and set speed
         gpg = EasyGoPiGo3()
         gpg.set_speed(0)
-
-        # Initialize distance sensor
-        myDistanceSensor = self.initialize_distance_sensor(gpg)
-
+        try:
+            # Initialize distance sensor
+            distance_sensor = gpg.init_distance_sensor()
+        except:
+            print('Failed to initialize distance sensor')
         # Initialize video capture and set resolution
-        video = VideoCapture(0)
+        try:
+            video = VideoCapture(0)
+        except:
+            print('Failed to set video')
+            
         video.set(CAP_PROP_FRAME_WIDTH,640)
         video.set(CAP_PROP_FRAME_HEIGHT,480)
         if video.isOpened() == False:
             print("Error opening video stream or file")
             raise Exception("Error opening video stream or file")
             
-        return video, gpg, myDistanceSensor
+        return video, gpg, distance_sensor
     
 
 
