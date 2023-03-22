f1bbb04bf4bfd6a672c95c2d018f6fbc432d9b4f
Nettking <m@nettking.no>
2023-03-22 23:31:20 +0100

    extracted method for send message

--- mqtt.py
+++ mqtt.py
@@ -1,34 +1,35 @@
 import paho.mqtt.client as mqtt
 
+def send_message(client, MQTT_TOPIC_SUB, message = "Hello, world!"):
+    # Publish a message to the specified topic
+    client.publish(MQTT_TOPIC_SUB, message)
+    print("Published message: " + message)
+
 # Define the callback function for receiving MQTT messages
 def on_message(client, MQTT_TOPIC_PUB, message):
     # Decode the message payload from bytes to string
     payload = message.payload.decode()
-    print("Received message: " + payload)
-
-    # Publish a response message back to the specified topic
     response = "Received: " + payload
-    client.publish(MQTT_TOPIC_PUB, response)
-    print("Published response: " + response)
+    print(response)
+    # Publish a response message back to the specified topic
+    send_message(client, MQTT_TOPIC_PUB, message = response)
+    
 
-def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT,MQTT_TOPIC_SUB, MQTT_TOPIC_PUB):
+def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT,MQTT_TOPIC_SUB, MQTT_TOPIC_PUB, message = "Hello, world!"):
     # Set up the MQTT client and connect to the broker
     client = mqtt.Client()
     client.connect(MQTT_BROKER_ADDR, MQTT_BROKER_PORT)
 
     # Set up the callback function for receiving messages
     client.on_message = on_message(client, MQTT_TOPIC_PUB, message)
-
     # Subscribe to the specified topic
     client.subscribe(MQTT_TOPIC_SUB)
-
-    # Publish a message to the specified topic
-    message = "Hello, world!"
-    client.publish(MQTT_TOPIC_SUB, message)
-    print("Published message: " + message)
+    
+    send_message(client, MQTT_TOPIC_SUB, message = message)
 
     # Start the MQTT client loop to process incoming messages
     client.loop_start()
+
 
 def connect_to_all_brokers():
     # Define the MQTT broker address and port
